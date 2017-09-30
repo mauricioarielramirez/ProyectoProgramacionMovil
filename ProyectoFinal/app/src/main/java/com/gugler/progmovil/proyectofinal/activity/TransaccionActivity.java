8518a755d8fc6c718210f4cd30ff192fbb2904e9
mauricioarielramirez <mauricioarielramirez@gmail.com>
2017-09-30 16:11:51 -0300

    Avance en activity Transaccion Issue relacionado: https://github.com/mauricioarielramirez/ProyectoProgramacionMovil/issues/9 - Se definió el comportamiento para la transacción editable - Se encontraron problemas con respecto a la transacción no editable - Bug relacionado: https://github.com/mauricioarielramirez/ProyectoProgramacionMovil/issues/12

--- ProyectoFinal/app/src/main/java/com/gugler/progmovil/proyectofinal/activity/TransaccionActivity.java
+++ ProyectoFinal/app/src/main/java/com/gugler/progmovil/proyectofinal/activity/TransaccionActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Space;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 import progmovil.gugler.com.pf.R;
 
@@ -18,6 +21,7 @@ public class TransaccionActivity extends BaseActivity {
 
     public final String TRANSACCION_NO_EDITABLE = "SoloConfirmarTransaccion";
     public final String TRANSACCION_EDITABLE = "EditarTransaccion";
+    private ArrayList<String> listaTipoTransaccion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +29,10 @@ public class TransaccionActivity extends BaseActivity {
         setContentView(R.layout.activity_transaccion);
         prepararStringSql();
         configurarInterface(TRANSACCION_NO_EDITABLE);
+        inicializarSpinnerTipoTransaccion();
     }
 
     private void configurarInterface(String modo) {
-        Spinner spnTransaccion = (Spinner)findViewById(R.id.spnTransaccion);
         Spinner spnTipo = (Spinner)findViewById(R.id.spnTipoTransaccion);
         Spinner spnCuenta = (Spinner)findViewById(R.id.spnCuentaAsociada);
         EditText txtImporte = (EditText) findViewById(R.id.txtImporte);
@@ -39,11 +43,14 @@ public class TransaccionActivity extends BaseActivity {
         LinearLayout layoutCancelar = (LinearLayout) findViewById(R.id.lytRightToolbar);
         Space spcLeft = (Space) findViewById(R.id.spcLeft);
         Space spcRight = (Space) findViewById(R.id.spcRight);
+        EditText txtTransaccion = (EditText) findViewById(R.id.txtTransaccion);
 
         //Modificar la toolbar
+        ActionBar actionBar = getSupportActionBar();
         switch (modo) {
             case TRANSACCION_NO_EDITABLE:
                 cargarDatosTransaccion();
+
                //MODIFICAR EL TOOLBAR
                 btnGuardar.setText("Confirmar");
                 Resources resource = getResources();
@@ -55,29 +62,54 @@ public class TransaccionActivity extends BaseActivity {
                 ViewGroup.LayoutParams paramsSpaceRight = spcRight.getLayoutParams();
                 paramsSpaceRight.width = 120;
 
-                spnTransaccion.setEnabled(false);
                 spnTipo.setEnabled(false);
                 spnCuenta.setEnabled(false);
-                txtImporte.setEnabled(false);
-            /*
-            BLOQUEAR SPINNERS Y EDITTEXT
-            CAMBIAR TITULOS
-            */
-            break;
-            case TRANSACCION_EDITABLE:
-            /*
-            HABILITAR SPINNERS Y EDITTEXT
-            CAMBIAR TITULOS
-            */
-            break;
-            default:
-                ActionBar actionBar = getSupportActionBar();
+
+
                 actionBar.setTitle("Transacción");
-                actionBar.setSubtitle("");
+                actionBar.setSubtitle("Confirmar transacción");
+                /*
+                BLOQUEAR SPINNERS Y EDITTEXT
+                CAMBIAR TITULOS
+                */
+                break;
+            case TRANSACCION_EDITABLE:
+
+                /*
+                HABILITAR SPINNERS Y EDITTEXT
+                CAMBIAR TITULOS
+                */
+                actionBar.setTitle("Transacción");
+                actionBar.setSubtitle("Editar transacción");
+                break;
         }
     }
 
+
     private void cargarDatosTransaccion(){
 
+    }
+
+    /**
+     * TEMPORALMENTE PARA PROBAR EL SPINNER DE CUENTAS
+     */
+    private void inicializarSpinnerTipoTransaccion(){
+        listaTipoTransaccion = new ArrayList<String>();
+        listaTipoTransaccion.add("Carga de crédito teléfono Claro de Eric Daniel Pennachini Corporativo");
+        listaTipoTransaccion.add("Compra diaria de comida");
+        listaTipoTransaccion.add("Depósito efectivo mensual");
+        listaTipoTransaccion.add("Carga de tarjebus");
+        listaTipoTransaccion.add("Pasaje diario SUBE");
+        Spinner spn1 = (Spinner) findViewById(R.id.spnTipoTransaccion);
+        Spinner spn2 = (Spinner) findViewById(R.id.spnCuentaAsociada);
+
+        try {
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, listaTipoTransaccion);
+
+            spn1.setAdapter(adapter);
+            spn2.setAdapter(adapter);
+        } catch(Exception ex) {
+            throw ex;
+        }
     }
 }
