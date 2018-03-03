5350f2ea9ce03d6a4bc9017320f9352bfc00176a
mauricioarielramirez <mauricioarielramirez@gmail.com>
2018-03-03 13:26:37 -0300

    Avances issue #23
    - Se creo la actvity ResultadoConsultaActivity
    - Se crearon los items y los headers (a modo de include) para la activity.

---
+++ ProyectoFinal/app/src/main/java/com/gugler/progmovil/proyectofinal/activity/ResultadoConsultaActivity.java
@@ -0,0 +1,15 @@
+package com.gugler.progmovil.proyectofinal.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import progmovil.gugler.com.pf.R;
+
+public class ResultadoConsultaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado_consulta);
+    }
+}
